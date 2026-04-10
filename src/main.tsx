230ccb29b3f213bf6b6767a1d1ca83ae0fd2d432
www-angZuxi <juiceboy327@gmail.com>
2026-04-11 01:55:25 +0800

    style: changed color theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
